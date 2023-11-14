8ae874d4f542705c0066cfeda580007196d1647b
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-14 12:17:30 -0800

    Use map to loop through nested array + solution works

--- src/components/Month.js
+++ src/components/Month.js
@@ -2,10 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Month(props) {
+
+
+
+
+
+
+const selectedProduce = 
+
+<ul>
+  {props.selection.map((select, index) =>
+    <li key={index}>{select}</li>
+  )}
+</ul>
+
   return (
     <React.Fragment>
       <h3>{props.month}</h3>
-      <p>{props.selection}</p>
+      {selectedProduce}
       <hr />
     </React.Fragment>
   );
