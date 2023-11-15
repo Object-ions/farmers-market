3ea136ded79bac57471fa8d37bfb69dffe791510
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-14 16:19:05 -0800

    Create MarketSchedule component + new features
    Co-authored-by: Moshe Atia <moshikoatia@gmail.com>
    Co-authored-by: Onur Kaymak <onurkaymak@outlook.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Header.css";
+import farmersMarket from "./../img/farmers-market.jpg"
 
 function Header(){
   return (
     <div className="header">
+      <img className="farmers-market" src={farmersMarket} alt="Farmers Market Logo" />
       <h1>Farmer's Market</h1>
     </div>
   );
